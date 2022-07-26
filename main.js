6fdc636e7b798f34f604ac489d3f816717d39f4b
Tomáš Hromník <tom.hromnik@gmail.com>
2022-07-26 15:41:32 +0200

    Fix when there is one long sentence without dot or end of sentence. Add more examples

--- main.js
+++ main.js
@@ -16,11 +16,14 @@ function truncate(text, limit = 255) {
     reduced = `${reduced.slice(0, limit - 3)}...`;
   }
 
-  return reduced || text;
+  return reduced || `${text.slice(0, limit - 3)}...`;
 }
 
 const str = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
 const str2 = "Very short sentence";
+const str3 = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
 console.log(truncate(str, 255));
 console.log(truncate(str, 455));
 console.log(truncate(str2, 100));
+console.log(truncate(str2, 10))
+console.log(truncate(str3, 80));
